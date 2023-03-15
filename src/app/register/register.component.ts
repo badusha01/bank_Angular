f0ae4167df7c02762af690c8715c5d0697ee5050
Badusha <badusha1818@gmail.com>
2023-03-15 12:37:38 +0530

    eighth commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Data, Router } from '@angular/router';
 import { DataService } from 'services/data.service';
 
@@ -9,22 +9,24 @@ import { DataService } from 'services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
- acno:any
- psw:any
- uname:any
+//  acno:any
+//  psw:any
+//  uname:any
  constructor(private ds: DataService, private router:Router,private fb:FormBuilder){}
 
 //  model for register form
 registerForm=this.fb.group({
-  acno:[''],
-  psw:[''],
-  uname:['']
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  psw:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]+')]],
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]]
 })
 
  register(){
-  var acno=this.acno
-  var psw=this.psw
-  var uname=this.uname
+  var acno=this.registerForm.value.acno
+  var psw=this.registerForm.value.psw
+  var uname=this.registerForm.value.uname
+  if(this.registerForm.valid){
+
 
   const result=this.ds.register(acno,uname,psw)
   if(result){
@@ -36,4 +38,8 @@ registerForm=this.fb.group({
   }
 
  }
+ else{
+  alert('Invalid form')
+ }
+}
 }
